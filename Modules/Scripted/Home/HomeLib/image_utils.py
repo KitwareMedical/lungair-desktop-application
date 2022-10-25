f047b1203c9077e8b845fd59fcb50b431aa48915
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-10-25 10:52:52 -0400

    STYLE: Update python code with indentation fixes
    This commit was adapted from Slicer/Slicer@9390be6bf
    
    This fixes all remaining pycodestyle E1 indentation error codes.
    
    Bulk of updates performed using autopep8 CLI
    with the following command:
    
      autopep8 --in-place --select E111,E114,E121,E123,E124,E128 $(git ls-files '*.py')
    
    See https://github.com/hhatto/autopep8#readme
    
    References:
    
    * Indentation is not a multiple of four
      See https://www.flake8rules.com/rules/E111.html
    
    * Indentation is not a multiple of four (comment)
      See https://www.flake8rules.com/rules/E114.html
    
    * Continuation line under-indented for hanging indent
      See https://www.flake8rules.com/rules/E121.html
    
    * Continuation line missing indentation or outdented
      See https://www.flake8rules.com/rules/E122.html
    
    * Closing bracket does not match indentation of opening bracket's line
      See https://www.flake8rules.com/rules/E123.html
    
    * Closing bracket does not match visual indentation
      See https://www.flake8rules.com/rules/E124.html
    
    * Continuation line under-indented for visual indent
      See https://www.flake8rules.com/rules/E128.html
    
    Co-authored-by: Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
    Co-authored-by: Lee Newberg <35611400+Leengit@users.noreply.github.com>

--- Modules/Scripted/Home/HomeLib/image_utils.py
+++ Modules/Scripted/Home/HomeLib/image_utils.py
@@ -8,107 +8,107 @@ from vtk.util.numpy_support import get_vtk_array_type, get_numpy_array_type
 
 
 def create_image_data_from_numpy_array(array, oriented : bool, copy = True):
-  """Create a vtk image data object from a numpy array.
-  A 2D numpy array will be turned into a single-axial-slice vtkImageData.
+    """Create a vtk image data object from a numpy array.
+    A 2D numpy array will be turned into a single-axial-slice vtkImageData.
 
-  Args:
-    array: a contiguous 2D numpy array of scalars to turn into a single-sliced 3D vtkImageData
-    oriented (bool): whether to return a vtkOrientedImageData instead of a vtkImageData
-    copy (bool): whether to copy the underlying data.
-      Keep this on unless you are sure the numpy array is going to remain a valid resource.
+    Args:
+      array: a contiguous 2D numpy array of scalars to turn into a single-sliced 3D vtkImageData
+      oriented (bool): whether to return a vtkOrientedImageData instead of a vtkImageData
+      copy (bool): whether to copy the underlying data.
+        Keep this on unless you are sure the numpy array is going to remain a valid resource.
 
-  Returns: vtkImageData or vtkOrientedImageData
-  """
+    Returns: vtkImageData or vtkOrientedImageData
+    """
 
-  # See the following for hints on how this works:
-  # https://github.com/Kitware/VTK/blob/master/Wrapping/Python/vtkmodules/util/numpy_support.py
+    # See the following for hints on how this works:
+    # https://github.com/Kitware/VTK/blob/master/Wrapping/Python/vtkmodules/util/numpy_support.py
 
-  if (len(array.shape)!=2):
-    raise ValueError(f"2D array was expected; got {len(array.shape)}D array")
+    if (len(array.shape)!=2):
+        raise ValueError(f"2D array was expected; got {len(array.shape)}D array")
 
-  # Get type, e.g. vtk.VTK_FLOAT
-  vtk_type = get_vtk_array_type(array.dtype)
+    # Get type, e.g. vtk.VTK_FLOAT
+    vtk_type = get_vtk_array_type(array.dtype)
 
-  # Ensure array was contiguous
-  assert(array.flags.c_contiguous)
+    # Ensure array was contiguous
+    assert(array.flags.c_contiguous)
 
-  # Create a vtkDataArray of the correct size
-  vtk_array = vtk.vtkDataArray.CreateDataArray(vtk_type)
-  vtk_array.SetNumberOfComponents(1)
-  vtk_array.SetNumberOfTuples(array.size)
+    # Create a vtkDataArray of the correct size
+    vtk_array = vtk.vtkDataArray.CreateDataArray(vtk_type)
+    vtk_array.SetNumberOfComponents(1)
+    vtk_array.SetNumberOfTuples(array.size)
 
-  # I'm not certain that the following assert is needed, but if it ever fails then look here for hints:
-  # https://github.com/Kitware/VTK/blob/0d344f312f143e7266ae10266f01470fb941ec96/Wrapping/Python/vtkmodules/util/numpy_support.py#L168
-  arr_dtype = get_numpy_array_type(vtk_type)
-  assert(np.issubdtype(array.dtype, arr_dtype) or array.dtype == np.dtype(arr_dtype))
+    # I'm not certain that the following assert is needed, but if it ever fails then look here for hints:
+    # https://github.com/Kitware/VTK/blob/0d344f312f143e7266ae10266f01470fb941ec96/Wrapping/Python/vtkmodules/util/numpy_support.py#L168
+    arr_dtype = get_numpy_array_type(vtk_type)
+    assert(np.issubdtype(array.dtype, arr_dtype) or array.dtype == np.dtype(arr_dtype))
 
-  # Set underlying data pointer of the vtkDataArray to point to the underlying data of the numpy array
-  array_flat = np.ravel(array)
-  vtk_array.SetVoidArray(array_flat, len(array_flat), 1)
+    # Set underlying data pointer of the vtkDataArray to point to the underlying data of the numpy array
+    array_flat = np.ravel(array)
+    vtk_array.SetVoidArray(array_flat, len(array_flat), 1)
 
-  # Deep copy the vtkDataArray so that it doesn't rely on the numpy resource to stay alive
-  if copy:
-    copy = vtk_array.NewInstance()
-    copy.DeepCopy(vtk_array)
-    vtk_array = copy
+    # Deep copy the vtkDataArray so that it doesn't rely on the numpy resource to stay alive
+    if copy:
+        copy = vtk_array.NewInstance()
+        copy.DeepCopy(vtk_array)
+        vtk_array = copy
 
-  # Create a vtkImageData and set our vtkDataArray to be its point data scalars
-  if oriented:
-    imageData = slicer.vtkOrientedImageData()
-  else:
-    imageData = vtk.vtkImageData()
-  imageData.SetDimensions([array.shape[0], array.shape[1], 1])
-  imageData.GetPointData().SetScalars(vtk_array)
+    # Create a vtkImageData and set our vtkDataArray to be its point data scalars
+    if oriented:
+        imageData = slicer.vtkOrientedImageData()
+    else:
+        imageData = vtk.vtkImageData()
+    imageData.SetDimensions([array.shape[0], array.shape[1], 1])
+    imageData.GetPointData().SetScalars(vtk_array)
 
-  return imageData
+    return imageData
 
 
 def create_volume_node_from_numpy_array(array, ijk_to_ras_directions, node_name : str):
-  """Create a volume node and add it to the scene.
+    """Create a volume node and add it to the scene.
 
-  Args:
-    array: a contiguous 2D numpy array of scalars to turn into a single-slice volume node
-    ijk_to_ras_directions: a list of three RAS-direction vectors, one for each axis of the array
-    node_name: string
+    Args:
+      array: a contiguous 2D numpy array of scalars to turn into a single-slice volume node
+      ijk_to_ras_directions: a list of three RAS-direction vectors, one for each axis of the array
+      node_name: string
 
-  Returns: the added vtkMRMLScalarVolumeNode
-  """
+    Returns: the added vtkMRMLScalarVolumeNode
+    """
 
-  imageData = create_image_data_from_numpy_array(array, oriented = False)
+    imageData = create_image_data_from_numpy_array(array, oriented = False)
 
-  # Create and add a volume node to the scene, setting our vtkImageData to be its underlying image data
-  volumeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode", node_name)
-  volumeNode.SetOrigin([0.,0.,0.])
-  volumeNode.SetSpacing([1.,1.,1.])
-  volumeNode.SetIJKToRASDirections(ijk_to_ras_directions)
-  volumeNode.SetAndObserveImageData(imageData)
-  volumeNode.CreateDefaultDisplayNodes()
-  defaultStorageNode = volumeNode.CreateDefaultStorageNode()
-  defaultStorageNode.UnRegister(None) # Needed to avoid memory leak. See https://github.com/Slicer/Slicer/issues/6099
+    # Create and add a volume node to the scene, setting our vtkImageData to be its underlying image data
+    volumeNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLScalarVolumeNode", node_name)
+    volumeNode.SetOrigin([0.,0.,0.])
+    volumeNode.SetSpacing([1.,1.,1.])
+    volumeNode.SetIJKToRASDirections(ijk_to_ras_directions)
+    volumeNode.SetAndObserveImageData(imageData)
+    volumeNode.CreateDefaultDisplayNodes()
+    defaultStorageNode = volumeNode.CreateDefaultStorageNode()
+    defaultStorageNode.UnRegister(None) # Needed to avoid memory leak. See https://github.com/Slicer/Slicer/issues/6099
 
-  return volumeNode
+    return volumeNode
 
 
 def create_segmentation_node_from_numpy_array(array, class_names : dict, node_name : str, vol_node):
-  """Create a segmentation node and add it to the scene.
-  Args:
-    array: a contiguous 2D numpy array consisting of discrete class labels
-    class_names (dict): a mapping from class labels (values in array) to name strings
-      name strings will be assigned to each segment
-    node_name (string): name of the segmentation node that will be created
-    vol_node: volume node that this segmentation should be associated to
+    """Create a segmentation node and add it to the scene.
+    Args:
+      array: a contiguous 2D numpy array consisting of discrete class labels
+      class_names (dict): a mapping from class labels (values in array) to name strings
+        name strings will be assigned to each segment
+      node_name (string): name of the segmentation node that will be created
+      vol_node: volume node that this segmentation should be associated to
 
-  Returns: the added vtkMRMLSegmentationNode
-  """
-  ijk_to_ras_directions = np.zeros((3,3))
-  vol_node.GetIJKToRASDirections(ijk_to_ras_directions)
+    Returns: the added vtkMRMLSegmentationNode
+    """
+    ijk_to_ras_directions = np.zeros((3,3))
+    vol_node.GetIJKToRASDirections(ijk_to_ras_directions)
 
-  segNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", node_name)
-  segNode.SetReferenceImageGeometryParameterFromVolumeNode(vol_node)
-  segNode.CreateDefaultDisplayNodes()
-  for class_label in class_names.keys():
-    binary_labelmap_array = (array==class_label).astype('int8')
-    orientedImageData = create_image_data_from_numpy_array(binary_labelmap_array, oriented = True)
-    orientedImageData.SetDirections(ijk_to_ras_directions)
-    segNode.AddSegmentFromBinaryLabelmapRepresentation(orientedImageData, class_names[class_label])
-  return segNode
+    segNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLSegmentationNode", node_name)
+    segNode.SetReferenceImageGeometryParameterFromVolumeNode(vol_node)
+    segNode.CreateDefaultDisplayNodes()
+    for class_label in class_names.keys():
+        binary_labelmap_array = (array==class_label).astype('int8')
+        orientedImageData = create_image_data_from_numpy_array(binary_labelmap_array, oriented = True)
+        orientedImageData.SetDirections(ijk_to_ras_directions)
+        segNode.AddSegmentFromBinaryLabelmapRepresentation(orientedImageData, class_names[class_label])
+    return segNode
--- Modules/Scripted/Home/HomeLib/segmentation_model.py
+++ Modules/Scripted/Home/HomeLib/segmentation_model.py
@@ -16,139 +16,139 @@ from .segmentation_post_processing import SegmentationPostProcessing
 
 
 class SegmentationModel:
-  class NoValue(enum.Enum):
-    def __repr__(self):
-      return '<%s.%s>' % (self.__class__.__name__, self.name)
+    class NoValue(enum.Enum):
+        def __repr__(self):
+            return '<%s.%s>' % (self.__class__.__name__, self.name)
 
-  class ModelSource(NoValue):
-      LOCAL_WEIGHTS = 'Locally saved model weights, without MONAI deploy'
-      LOCAL_DEPLOY = 'MONAI Deploy with locally saved model weights'
-      DOCKER_DEPLOY = 'MONAI Deploy with docker image'
+    class ModelSource(NoValue):
+        LOCAL_WEIGHTS = 'Locally saved model weights, without MONAI deploy'
+        LOCAL_DEPLOY = 'MONAI Deploy with locally saved model weights'
+        DOCKER_DEPLOY = 'MONAI Deploy with docker image'
 
-  def __init__(self, load_pth_path, backend_to_use):
-    """
-    This class provides a way to interface with a lung segmentation model trained in MONAI.
-    It loads the model on construction, and it handles loading and transforming
-    images and running inference.
-    """
-    self.model_source = backend_to_use
+    def __init__(self, load_pth_path, backend_to_use):
+        """
+        This class provides a way to interface with a lung segmentation model trained in MONAI.
+        It loads the model on construction, and it handles loading and transforming
+        images and running inference.
+        """
+        self.model_source = backend_to_use
 
-    self.load_pth_path = load_pth_path
-    # For save_zip_path, remove trailing .pth if present; append .zip
-    self.save_zip_path = re.sub(r"\.pth$", "", self.load_pth_path) + ".zip"
+        self.load_pth_path = load_pth_path
+        # For save_zip_path, remove trailing .pth if present; append .zip
+        self.save_zip_path = re.sub(r"\.pth$", "", self.load_pth_path) + ".zip"
 
-    if self.model_source == self.ModelSource.LOCAL_WEIGHTS:
-      model_dict = torch.load(self.load_pth_path, map_location=torch.device('cpu'))
+        if self.model_source == self.ModelSource.LOCAL_WEIGHTS:
+            model_dict = torch.load(self.load_pth_path, map_location=torch.device('cpu'))
 
-      self.seg_net = model_dict['model']
-      self.learning_rate = model_dict['learning_rate']
-      self.training_losses = model_dict['training_losses']
-      self.validation_losses = model_dict['validation_losses']
-      self.epoch_number = model_dict['epoch_number']
-      self.best_validation_loss = model_dict['best_validation_loss']
-      self.best_validation_epoch = model_dict['best_validation_epoch']
-      self.image_size = model_dict['image_size']
+            self.seg_net = model_dict['model']
+            self.learning_rate = model_dict['learning_rate']
+            self.training_losses = model_dict['training_losses']
+            self.validation_losses = model_dict['validation_losses']
+            self.epoch_number = model_dict['epoch_number']
+            self.best_validation_loss = model_dict['best_validation_loss']
+            self.best_validation_epoch = model_dict['best_validation_epoch']
+            self.image_size = model_dict['image_size']
 
-      # Transforms a given image to the input format expected by the segmentation network
-      self.transform = monai.transforms.Compose([
-        monai.transforms.CastToType(dtype=np.float32), # TODO dtype should have been included in the model_dict
-        monai.transforms.AddChannel(),
-        monai.transforms.Resize(
-          spatial_size=(self.image_size,self.image_size),
-          mode = 'bilinear',
-          align_corners=False
-        ),
-        monai.transforms.ToTensor()
-      ])
+            # Transforms a given image to the input format expected by the segmentation network
+            self.transform = monai.transforms.Compose([
+                monai.transforms.CastToType(dtype=np.float32), # TODO dtype should have been included in the model_dict
+                monai.transforms.AddChannel(),
+                monai.transforms.Resize(
+                    spatial_size=(self.image_size,self.image_size),
+                    mode = 'bilinear',
+                    align_corners=False
+                ),
+                monai.transforms.ToTensor()
+            ])
 
-      self.seg_post_process = SegmentationPostProcessing()
+            self.seg_post_process = SegmentationPostProcessing()
 
-    if self.model_source in (self.ModelSource.LOCAL_DEPLOY, self.ModelSource.DOCKER_DEPLOY) and not os.path.exists(self.save_zip_path):
-      # Write out a TorchScript version of the model, for use in MONAI Deploy.
-      model_dict = torch.load(self.load_pth_path, map_location=torch.device('cpu'))
-      seg_net = model_dict['model']
-      # set dropout and batch normalization layers to evaluation mode before running
-      # inference
-      seg_net.eval()
-      torch.jit.script(seg_net).save(self.save_zip_path)
+        if self.model_source in (self.ModelSource.LOCAL_DEPLOY, self.ModelSource.DOCKER_DEPLOY) and not os.path.exists(self.save_zip_path):
+            # Write out a TorchScript version of the model, for use in MONAI Deploy.
+            model_dict = torch.load(self.load_pth_path, map_location=torch.device('cpu'))
+            seg_net = model_dict['model']
+            # set dropout and batch normalization layers to evaluation mode before running
+            # inference
+            seg_net.eval()
+            torch.jit.script(seg_net).save(self.save_zip_path)
 
-  def run_inference(self, img):
-    """
-    Execute segmentation model on a chest xray, given as an array of shape (height, width).
+    def run_inference(self, img):
+        """
+        Execute segmentation model on a chest xray, given as an array of shape (height, width).
 
-    The image axes are assumed to be in "matrix" order, with the origin in the upper left of the image:
-    - The 0 axis should go along the height of the radiograph, towards patient-inferior
-    - The 1 axis should go along the width of the radiograph, towards patient-left/image-right
+        The image axes are assumed to be in "matrix" order, with the origin in the upper left of the image:
+        - The 0 axis should go along the height of the radiograph, towards patient-inferior
+        - The 1 axis should go along the width of the radiograph, towards patient-left/image-right
 
-    The segmentation model should include post-processing but this is SKIPPED for now; TODO.
-    Currently it's just a segmentation network.
+        The segmentation model should include post-processing but this is SKIPPED for now; TODO.
+        Currently it's just a segmentation network.
 
-    Returns (seg_mask, model_to_img_matrix), where:
-      seg_mask is a torch tensor of shape (height, width), a binary label mask indicating the lung field
-      model_to_img_matrix is a 2D numpy array representing the linear transform from the coordinate space of the segmentation model
-        output to the original coordinate space of the given array img.
-    """
-    if len(img.shape) != 2:
-      raise ValueError("img must be a 2D array")
+        Returns (seg_mask, model_to_img_matrix), where:
+          seg_mask is a torch tensor of shape (height, width), a binary label mask indicating the lung field
+          model_to_img_matrix is a 2D numpy array representing the linear transform from the coordinate space of the segmentation model
+            output to the original coordinate space of the given array img.
+        """
+        if len(img.shape) != 2:
+            raise ValueError("img must be a 2D array")
 
-    if self.model_source == self.ModelSource.LOCAL_WEIGHTS:
-      self.seg_net.eval()
-      img_input = self.transform(img)
-      seg_net_output = self.seg_net(img_input.unsqueeze(0))[0]
+        if self.model_source == self.ModelSource.LOCAL_WEIGHTS:
+            self.seg_net.eval()
+            img_input = self.transform(img)
+            seg_net_output = self.seg_net(img_input.unsqueeze(0))[0]
 
-      # assumption at the moment is that we have 2-channel image out (i.e. purely binary segmentation was done)
-      assert(seg_net_output.shape[0]==2)
+            # assumption at the moment is that we have 2-channel image out (i.e. purely binary segmentation was done)
+            assert(seg_net_output.shape[0]==2)
 
-      _, max_indices = seg_net_output.max(dim=0)
-      seg_mask = (max_indices==1).type(torch.uint8)
+            _, max_indices = seg_net_output.max(dim=0)
+            seg_mask = (max_indices==1).type(torch.uint8)
 
-      model_to_img_matrix = np.diag(np.array(img.shape)/self.image_size)
+            model_to_img_matrix = np.diag(np.array(img.shape)/self.image_size)
 
-      # TODO skipping post processing because post processing causes crash due to ITK
-      # python issues seg_processed = self.seg_post_process(seg_mask)
-      seg_processed = seg_mask
+            # TODO skipping post processing because post processing causes crash due to ITK
+            # python issues seg_processed = self.seg_post_process(seg_mask)
+            seg_processed = seg_mask
 
-    if self.model_source in (self.ModelSource.LOCAL_DEPLOY, self.ModelSource.DOCKER_DEPLOY):
-      # Communicate with monai deploy via files.  Locations are:
-      input_dir_path = tempfile.mkdtemp()
-      output_dir_path = tempfile.mkdtemp()
-      input_file_path = os.path.join(input_dir_path, "input.png")
-      output_mask_path = os.path.join(output_dir_path, "mask.png")
-      output_model_to_img_matrix_path = os.path.join(output_dir_path, "model_to_img_matrix.npy")
+        if self.model_source in (self.ModelSource.LOCAL_DEPLOY, self.ModelSource.DOCKER_DEPLOY):
+            # Communicate with monai deploy via files.  Locations are:
+            input_dir_path = tempfile.mkdtemp()
+            output_dir_path = tempfile.mkdtemp()
+            input_file_path = os.path.join(input_dir_path, "input.png")
+            output_mask_path = os.path.join(output_dir_path, "mask.png")
+            output_model_to_img_matrix_path = os.path.join(output_dir_path, "model_to_img_matrix.npy")
 
-      # Write input file
-      img_pil = PIL.Image.fromarray(img)
-      img_pil = img_pil.convert("L")
-      img_pil.save(input_file_path)
+            # Write input file
+            img_pil = PIL.Image.fromarray(img)
+            img_pil = img_pil.convert("L")
+            img_pil.save(input_file_path)
 
-      # Run monai-deploy
-      monai_deploy_path = shutil.which("monai-deploy")
-      if self.model_source == self.ModelSource.LOCAL_DEPLOY:
-        deploy_app_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "deploy_app.py")
-        process_image_command = [monai_deploy_path,
-                                 "exec", deploy_app_path,
-                                 "-m", self.save_zip_path,
-                                 "-i", input_dir_path,
-                                 "-o", output_dir_path]
-      if self.model_source == self.ModelSource.DOCKER_DEPLOY:
-        # TODO: Unfortunately, this docker image appears to require CUDA>=11.3.
-        docker_image = "ghcr.io/kitwaremedical/lungair-desktop-application/lung_air_model_deploy:latest"
-        # Note: to *create* the docker image, use
-        # f"{monai_deploy_path} package {deploy_app_path} --tag {docker_image} --model {self.save_zip_path}"
-        # f"echo {CR_PAT} | docker login -u {User} --password-stdin ghcr.io"
-        # f"docker push {docker_image}"
-        process_image_command = [monai_deploy_path,
-                                 "run", docker_image,
-                                 input_dir_path, output_dir_path]
-      proc = slicer.util.launchConsoleProcess(process_image_command, useStartupEnvironment=False)
-      slicer.util.logProcessOutput(proc)
+            # Run monai-deploy
+            monai_deploy_path = shutil.which("monai-deploy")
+            if self.model_source == self.ModelSource.LOCAL_DEPLOY:
+                deploy_app_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "deploy_app.py")
+                process_image_command = [monai_deploy_path,
+                                         "exec", deploy_app_path,
+                                         "-m", self.save_zip_path,
+                                         "-i", input_dir_path,
+                                         "-o", output_dir_path]
+            if self.model_source == self.ModelSource.DOCKER_DEPLOY:
+                # TODO: Unfortunately, this docker image appears to require CUDA>=11.3.
+                docker_image = "ghcr.io/kitwaremedical/lungair-desktop-application/lung_air_model_deploy:latest"
+                # Note: to *create* the docker image, use
+                # f"{monai_deploy_path} package {deploy_app_path} --tag {docker_image} --model {self.save_zip_path}"
+                # f"echo {CR_PAT} | docker login -u {User} --password-stdin ghcr.io"
+                # f"docker push {docker_image}"
+                process_image_command = [monai_deploy_path,
+                                         "run", docker_image,
+                                         input_dir_path, output_dir_path]
+            proc = slicer.util.launchConsoleProcess(process_image_command, useStartupEnvironment=False)
+            slicer.util.logProcessOutput(proc)
 
-      # Read output files
-      seg_processed = torch.from_numpy(np.asarray(PIL.Image.open(output_mask_path)))
-      model_to_img_matrix = np.load(output_model_to_img_matrix_path)
+            # Read output files
+            seg_processed = torch.from_numpy(np.asarray(PIL.Image.open(output_mask_path)))
+            model_to_img_matrix = np.load(output_model_to_img_matrix_path)
 
-      # Clean up files that were used for communication
-      shutil.rmtree(output_dir_path)
-      shutil.rmtree(input_dir_path)
+            # Clean up files that were used for communication
+            shutil.rmtree(output_dir_path)
+            shutil.rmtree(input_dir_path)
 
-    return seg_processed, model_to_img_matrix
+        return seg_processed, model_to_img_matrix
