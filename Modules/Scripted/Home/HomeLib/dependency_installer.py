cd2109805fcd3d089ea4e3adf364c03d695a53c2
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2022-10-25 10:52:52 -0400

    STYLE: Update python code with indentation fixes
    This commit was adapted from Slicer/Slicer@9390be6bf
    
    This fixes all remaining pycodestyle E1 indentation error codes.
    
    Bulk of updates performed using autopep8 CLI
    with the following command:
    
      autopep8 --in-place --select E111,E114,E121,E123,E124,E128 $(git ls-files '*.py')
    
    See https://github.com/hhatto/autopep8#readme
    
    References:
    
    * Indentation is not a multiple of four
      See https://www.flake8rules.com/rules/E111.html
    
    * Indentation is not a multiple of four (comment)
      See https://www.flake8rules.com/rules/E114.html
    
    * Continuation line under-indented for hanging indent
      See https://www.flake8rules.com/rules/E121.html
    
    * Continuation line missing indentation or outdented
      See https://www.flake8rules.com/rules/E122.html
    
    * Closing bracket does not match indentation of opening bracket's line
      See https://www.flake8rules.com/rules/E123.html
    
    * Closing bracket does not match visual indentation
      See https://www.flake8rules.com/rules/E124.html
    
    * Continuation line under-indented for visual indent
      See https://www.flake8rules.com/rules/E128.html
    
    Co-authored-by: Ebrahim Ebrahim <ebrahim.ebrahim@kitware.com>
    Co-authored-by: Lee Newberg <35611400+Leengit@users.noreply.github.com>

--- Modules/Scripted/Home/HomeLib/dependency_installer.py
+++ Modules/Scripted/Home/HomeLib/dependency_installer.py
@@ -13,53 +13,53 @@ class BusyCursor:
 
 
 def check_and_install_package(module_names, pip_install_name, pre_install_hook = None):
-  """
-  Check if given module can be imported, and if not then prompt user to possibly attempt an install.
+    """
+    Check if given module can be imported, and if not then prompt user to possibly attempt an install.
 
-  Args:
-    module_names: a list of strings for the modules for which import needs to succeed
-    pip_install_name: the name of the package to install using pip in order to make the import succeed
-      (or whatever text should follow "pip install" in the installation command)
-    pre_install_hook: an optional callable that will be called before installation, in the event that installation is going to take place
-  Returns whether the import can succeed at the end.
-  """
-  try:
-    modules = []
-    for module_name in module_names:
-      modules.append(importlib.import_module(module_name))
-    version_text = "\n".join(
-        [
-            f"  {module_name} version: {module.__version__}"
-            for module, module_name in zip(modules, module_names)
-            if hasattr(module, "__version__")
-        ]
-    )
-    slicer.util.infoDisplay("Modules found!\n" + version_text, "Modules Found")
-    return True
-  except ModuleNotFoundError as e1:
-    wantInstall = slicer.util.confirmYesNoDisplay(f"Package was not found. Install it?\nDetails of missing import: {e1}", "Missing Dependency")
-    if wantInstall:
-      if pre_install_hook is not None:
-        pre_install_hook()
-      with BusyCursor():
-        slicer.util.pip_install(pip_install_name)
-      try:
+    Args:
+      module_names: a list of strings for the modules for which import needs to succeed
+      pip_install_name: the name of the package to install using pip in order to make the import succeed
+        (or whatever text should follow "pip install" in the installation command)
+      pre_install_hook: an optional callable that will be called before installation, in the event that installation is going to take place
+    Returns whether the import can succeed at the end.
+    """
+    try:
+        modules = []
         for module_name in module_names:
-          importlib.import_module(module_name)
-        slicer.util.infoDisplay("Finished installing.", "Install Success")
+            modules.append(importlib.import_module(module_name))
+        version_text = "\n".join(
+            [
+                f"  {module_name} version: {module.__version__}"
+                for module, module_name in zip(modules, module_names)
+                if hasattr(module, "__version__")
+            ]
+        )
+        slicer.util.infoDisplay("Modules found!\n" + version_text, "Modules Found")
         return True
-      except ModuleNotFoundError as e2:
-        slicer.util.errorDisplay("Unable to install package. Check the console for details.", "Install Error")
-        print(e2)
-        return False
+    except ModuleNotFoundError as e1:
+        wantInstall = slicer.util.confirmYesNoDisplay(f"Package was not found. Install it?\nDetails of missing import: {e1}", "Missing Dependency")
+        if wantInstall:
+            if pre_install_hook is not None:
+                pre_install_hook()
+            with BusyCursor():
+                slicer.util.pip_install(pip_install_name)
+            try:
+                for module_name in module_names:
+                    importlib.import_module(module_name)
+                slicer.util.infoDisplay("Finished installing.", "Install Success")
+                return True
+            except ModuleNotFoundError as e2:
+                slicer.util.errorDisplay("Unable to install package. Check the console for details.", "Install Error")
+                print(e2)
+                return False
 
 
 # A pre-install step for monai, where we use light-the-torch to install torch more carefully:
 # with light-the-torch, the computation backend is auto-detected from the available hardware preferring CUDA over CPU.
 def monai_pre_install():
-  with BusyCursor():
-    slicer.util.pip_install('light-the-torch')
-    slicer.util._executePythonModule('light_the_torch', ['install', 'monai'])
+    with BusyCursor():
+        slicer.util.pip_install('light-the-torch')
+        slicer.util._executePythonModule('light_the_torch', ['install', 'monai'])
 
 
 check_and_install_itk = functools.partial(check_and_install_package, ["itk"], "itk")
